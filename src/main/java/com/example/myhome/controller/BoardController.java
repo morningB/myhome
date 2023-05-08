d9bd7050b345e3e2a85a7ac6f6af319f1fe28c3d
morningB <yjw001205@gmail.com>
2023-05-08 16:00:49 +0900

    헤걀!

--- src/main/java/com/example/myhome/controller/BoardController.java
+++ src/main/java/com/example/myhome/controller/BoardController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,15 +17,21 @@ import java.util.List;
 public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
+
     @GetMapping("/list")
     public String list(Model model) {
-
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
     }
     @GetMapping("/form")
     public String form(Model model){
+        model.addAttribute("board",new Board());
         return "board/form";
+    }
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board){
+
+        return "result";
     }
 }
